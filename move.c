99a9318c7925151d89a894d6453c22efcb009294
Mohamed Jama <m.d.jama@hotmail.com>
2017-04-13 10:29:31 +0100

    change &board to board.

--- move.c
+++ move.c
@@ -10,44 +10,50 @@
 		switch(choice)
 		{
 			case 1:{
+				//Moving UP.
 				int row_n, column_n;
 				row_n = cases[i].place->row;
 				column_n = cases[i].place->column;
-				cases[i].place->player_Identifier=-1;
 				printf("%s\n(%d, %d)\n",cases[i].name_one, cases[i].place->row, cases[i].place->column);
+				cases[i].place->player_Identifier=-1;
 				cases[i].place = board[row_n][column_n].up;
 				cases[i].place->player_Identifier=i+20;
 				printf("%s\n(%d, %d)\n",cases[i].name_one, cases[i].place->row, cases[i].place->column);
 				break;
 			}
 			case 2:{
+				//Moving Down.
 				int row_n, column_n;
 				row_n = cases[i].place->row;
 				column_n = cases[i].place->column;
-				cases[i].place->player_Identifier=-1;
 				printf("%s\n(%d, %d)\n",cases[i].name_one, cases[i].place->row, cases[i].place->column);
-				cases[i].place = &board[row_n][column_n].down;
+				cases[i].place->player_Identifier=-1;
+				cases[i].place = board[row_n][column_n].down;
+				cases[i].place->player_Identifier=i+20;
 				printf("%s\n(%d, %d)\n",cases[i].name_one, cases[i].place->row, cases[i].place->column);
 				break;
 			}
 			case 3:{
+				//Move Right
 				int row_n, column_n;
 				row_n = cases[i].place->row;
 				column_n = cases[i].place->column;
-				cases[i].place->player_Identifier=-1;
 				printf("%s\n(%d, %d)\n",cases[i].name_one, cases[i].place->row, cases[i].place->column);
-				cases[i].place = &board[row_n][column_n].right;
+				cases[i].place->player_Identifier=-1;
+				cases[i].place = board[row_n][column_n].right;
 				cases[i].place->player_Identifier=i+20;
 				printf("%s\n(%d, %d)\n",cases[i].name_one, cases[i].place->row, cases[i].place->column);
 				break;
 			}
 			case 4:{
+				//Move Left.
 				int row_n, column_n;
 				row_n = cases[i].place->row;
 				column_n = cases[i].place->column;
-				cases[i].place->player_Identifier=-1;
 				printf("%s\n(%d, %d)\n",cases[i].name_one, cases[i].place->row, cases[i].place->column);
-				cases[i].place = &board[row_n][column_n].left;
+				printf("Next: (%d,%d)\n", board[row_n][column_n].left->row, board[row_n][column_n].left->column);
+				cases[i].place->player_Identifier=-1;
+				cases[i].place = board[row_n][column_n].left;
 				cases[i].place->player_Identifier=i+20;
 				printf("%s\n(%d, %d)\n",cases[i].name_one, cases[i].place->row, cases[i].place->column);
 				break;
